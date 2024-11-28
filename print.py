8c38c07242396154f954a11889d916411706700e
Label-labs-ENG <training.english@labellabs.ai>
2024-11-28 13:42:22 +0530

    Added

--- print.py
+++ print.py
@@ -1,6 +1,10 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 print('hello')
+=======
+print('hello world')
+>>>>>>> Stashed changes
 =======
 print('hello world')
 >>>>>>> Stashed changes
