74fae11f23b0fb8e0ba274336867ef448b358fcf
Label-labs-TAM <training.tamil@labellabs.ai>
2024-11-28 14:23:07 +0530

    Update print.py

--- print.py
+++ print.py
@@ -1,17 +1,1 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-print('hello')
-=======
-print('hello world')
->>>>>>> Stashed changes
-=======
-print('hello world')
->>>>>>> Stashed changes
-=======
-print('hello world')
->>>>>>> Stashed changes
-=======
-print('hello world')
->>>>>>> Stashed changes
+print('Python')
